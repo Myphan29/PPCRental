ad6c73ce6cd81acba347648b47ab666ff91fc84e
Myphan29 <tramy290997@gmail.com>
2017-11-10 13:21:32 +0700

    Created Project and layout

---
+++ PPCRental/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PPCRental.Startup))]
+namespace PPCRental
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
