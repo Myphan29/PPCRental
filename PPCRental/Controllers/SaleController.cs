b8a98896c53e0507197540585ef4c95dd8210625
Minh Minh <Admin@MM>
2017-11-19 21:02:09 +0700

    Update View Agency Project

---
+++ PPCRental/Controllers/SaleController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PPCRental.Models;
+using PagedList;
+
+namespace PPCRental.Controllers
+{
+    public class SaleController : Controller
+    {
+        PPCRentalEntities model = new PPCRentalEntities();
+        // GET: Sale
+        public ActionResult Index()
+        {
+            //var property = model.PROPERTies.ToList();
+            //return View(property);
+            return View();
+        }
+        
+        public PartialViewResult Getpaging(int? page)
+        {
+            var property = model.PROPERTies.ToList();
+            int pageSize = 6;
+            int pageNumber = (page ?? 1);
+            return PartialView("Getpaging", property.ToPagedList(pageNumber, pageSize));
+        }
+    }
+}
